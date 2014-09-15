87ecfa76f505af9ee2292613288ebea58d498584
caijinhua <caijinhua@os-easy.com>
2014-09-15 13:58:39 +0800

    update redis api

--- hgsf/db/redis_api.py
+++ hgsf/db/redis_api.py
@@ -9,16 +9,33 @@ class RedisApi:
         self.hostname = ""
         self.port = 6379
         self.db = 0
+        self.pool = None
         self.connection = None
 
-    def connect(self, hostname, port = 6379, db = 0):
+    def connect(self, hostname='localhost', port = 6379, db = 0):
         self.hostname = hostname
         self.port = port
         self.db = db
-        self.connection = redis.StrictRedis(hostname, port, db)
+        self.pool = redis.ConnectionPool(host = self.hostname, port = self.port, db = self.db)
+        self.connection = redis.Redis(connection_pool = self.pool)
 
     def set(self, key, value):
         self.connection.set(key, value)
 
+    def append(self, key, value):
+        self.connection.append(key, value)
+
+    def delete(self, key):
+        return self.connection.delete(key)
+
     def get(self, key):
         return self.connection.get(key)
+
+    def strlen(self, key):
+        return self.connection.strlen(key)
+
+    def ttl(self, key):
+        return self.connection.ttl(key)
+
+    def dbsize(self):
+        return self.connection.dbsize()
